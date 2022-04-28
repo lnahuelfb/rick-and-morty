496c668ea8f5876f30857c3b5ea2e90f45e684b7
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-04-28 01:57:01 -0300

    Agregadas mejoras esteticas

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,7 +5,7 @@ import '../styles/NotFound.css'
 export default function NotFound() {
   return (
     <div className='nf-container'>
-      <h1>Error 404: Personaje no encontrado</h1>
+      <h1>Error 404: <br/> Personaje no encontrado</h1>
       <img src={rickymorty} alt="Not Found" />
     </div>
   )
